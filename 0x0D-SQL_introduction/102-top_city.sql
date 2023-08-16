6811a3cbedb83214b5e040f83d604966c5838e5c
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-08-16 19:39:05 +0800

    Update 102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,7 +1,2 @@
 -- Display the top 3 cities' temperatures during July and August, ordered by temperature (descending)
-SELECT city, ROUND(AVG(temperature), 4) AS avg_temp
-FROM temperatures
-WHERE (MONTH(date) = 7 OR MONTH(date) = 8)
-GROUP BY city
-ORDER BY avg_temp DESC
-LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY AVG(value) DESC LIMIT 3;
