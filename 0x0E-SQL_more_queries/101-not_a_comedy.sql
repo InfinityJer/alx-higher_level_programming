96d910296e575558a7b2f2d627718f0aa850c7ee
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-08-17 05:25:18 +0800

    Update 101-not_a_comedy.sql

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,7 +1,10 @@
 -- List all shows without the genre Comedy
 SELECT tv_shows.title
-FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name != 'Comedy' OR tv_genres.name IS NULL
-ORDER BY tv_shows.title ASC;
+	FROM tv_genres
+	INNER JOIN tv_show_genres
+	ON tv_genres.name = "Comedy" AND tv_show_genres.genre_id = tv_genres.id
+	RIGHT JOIN tv_shows
+	ON tv_show_genres.show_id = tv_shows.id
+	WHERE tv_show_genres.show_id IS NULL
+	GROUP BY tv_shows.title
+	ORDER BY tv_shows.title ASC;
