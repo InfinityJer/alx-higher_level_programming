e8516223670432a29d37f0b7674b448462c1f75c
InfintyJer <jclaris6@gmail.com>
2023-08-16 10:09:13 -0700

    script that creates the MySQL server user user_0d_1.

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- Create the MySQL server user user_0d_1 if it doesn't exist
+-- Grant all privileges to user_0d_1 on the entire MySQL server
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
