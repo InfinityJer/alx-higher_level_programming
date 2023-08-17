971516710d9ddd00349d149f8c1b058fbc25c12c
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-08-17 18:57:13 +0800

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- Create the MySQL server user user_0d_1 if it doesn't exist
--- Grant all privileges to user_0d_1 on the entire MySQL server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1 on the entire MySQL server
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
