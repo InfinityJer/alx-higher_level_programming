e50e0a610076a96eaaa570ed6c2575012cc10f8d
InfintyJer <jclaris6@gmail.com>
2023-09-14 08:35:55 -0700

    script that concats 2 files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -11,30 +11,46 @@ const sourceFile1 = args[0];
 const sourceFile2 = args[1];
 const destinationFile = args[2];
 
-// Read the content of sourceFile1
-fs.readFile(sourceFile1, 'utf8', (err, data1) => {
+// Check if sourceFile1 exists and is readable
+fs.access(sourceFile1, fs.constants.R_OK, (err) => {
   if (err) {
     console.error(`Error reading ${sourceFile1}: ${err.message}`);
     process.exit(1);
   }
 
-  // Read the content of sourceFile2
-  fs.readFile(sourceFile2, 'utf8', (err, data2) => {
+  // Check if sourceFile2 exists and is readable
+  fs.access(sourceFile2, fs.constants.R_OK, (err) => {
     if (err) {
       console.error(`Error reading ${sourceFile2}: ${err.message}`);
       process.exit(1);
     }
 
-    // Concatenate the contents of data1 and data2
-    const concatenatedData = data1 + data2;
-
-    // Write the concatenated data to the destinationFile
-    fs.writeFile(destinationFile, concatenatedData, 'utf8', (err) => {
+    // Read the content of sourceFile1
+    fs.readFile(sourceFile1, 'utf8', (err, data1) => {
       if (err) {
-        console.error(`Error writing to ${destinationFile}: ${err.message}`);
+        console.error(`Error reading ${sourceFile1}: ${err.message}`);
         process.exit(1);
       }
-      console.log(`Concatenated files ${sourceFile1} and ${sourceFile2} into ${destinationFile}`);
+
+      // Read the content of sourceFile2
+      fs.readFile(sourceFile2, 'utf8', (err, data2) => {
+        if (err) {
+          console.error(`Error reading ${sourceFile2}: ${err.message}`);
+          process.exit(1);
+        }
+
+        // Concatenate the contents of data1 and data2
+        const concatenatedData = data1 + data2;
+
+        // Write the concatenated data to the destinationFile
+        fs.writeFile(destinationFile, concatenatedData, 'utf8', (err) => {
+          if (err) {
+            console.error(`Error writing to ${destinationFile}: ${err.message}`);
+            process.exit(1);
+          }
+          console.log(`Concatenated files ${sourceFile1} and ${sourceFile2} into ${destinationFile}`);
+        });
+      });
     });
   });
 });
