dcfc703d708cdb47f2bdeba8e58137c58f535fe6
InfintyJer <jclaris6@gmail.com>
2023-09-14 07:41:34 -0700

    class Rectangle that defines a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      return {}; // Return an empty object if w or h is not a positive integer
+    }
+
+    this.width = w;
+    this.height = h;
+  }
+}
+
+module.exports = Rectangle;
