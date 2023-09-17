e79b49824b99201e8751259df2f1b591a9fa9bfa
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-09-17 23:32:26 +0800

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,40 +1,16 @@
 #!/usr/bin/python3
-import sys
+"""
+Script that lists all states from the database hbtn_0e_0_usa.
+"""
+from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    # Check if three command-line arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Extract command-line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Connect to the MySQL server
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
-    )
-
-    # Create a cursor object to interact with the database
-    cursor = db.cursor()
-
-    # Execute the SQL query to select all states
-    cursor.execute("SELECT * FROM states ORDER BY id")
-
-    # Fetch all the rows from the result set
-    states = cursor.fetchall()
-
-    # Print the results
-    for state in states:
-        print(state)
-
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
+    db = MySQLdb.connect(host="localhost",
+                         user=argv[1],
+                         port=3306,
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states` ORDER BY states.id ASC")
+    [print(state) for state in cur.fetchall()]
