d74e6db209fce589cb8f6b618932737005cb7088
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-09-28 23:52:03 +0800

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send the request and include a custom header to trigger the response
-curl -s -H "Origin: HolbertonSchool" -X PUT -d "user_id=98" "0.0.0.0:5000/catch_me"
+# makes a request to 0.0.0.0:5000/catch_me and displays the response
+curl -X PUT -L -H "Origin: HolbertonSchool" -s 0.0.0.0:5000/catch_me -d 'user_id=98'
