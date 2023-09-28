163250ca13b4da2d659ecd568bbde99459b028ca
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-09-28 23:20:58 +0800

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-# This script sends a POST request to the URL with specific parameters and displays the body of the response
-
-# URL to send the POST request to
-url="$1"
-
-# Define the POST data as URL-encoded form parameters
-data="email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD"
-
-# Send the POST request and display the response body
-curl -s -X POST -d "$data" "$url"
+# sends a POST request to the URL with specific parameters and displays the body of the response
+curl -sd 'email=test@gmail.com&subject=I will always be here for PLD' "$1"
