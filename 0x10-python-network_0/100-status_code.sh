ad6e2ddd7693eb9ea165bbb1235bf1b96f855aab
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-09-28 23:43:33 +0800

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# This script sends a request to a URL and displays only the status code of the response
 # Send the request and display only the status code
-curl -s -o /dev/null -w "%{http_code}" "$1"i
+curl -s -o /dev/null -w "%{http_code}" "$1"
