9235d9f572e39d9c2ad9b404e69efa75ff059f5c
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-09-29 19:42:42 +0800

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Script fetches https://alx-intranet.hbtn.io/status
+Python Script fetches https://alx-intranet.hbtn.io/status
 """
-import urllib.request
+import request
 
 if __name__ == "__main__":
     r = requests.get('https://alx-intranet.hbtn.io/status')
