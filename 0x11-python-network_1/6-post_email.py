739ce8b864cac1fac54de8de9a4ac14637b001c8
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-09-29 19:56:24 +0800

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,23 +1,13 @@
 #!/usr/bin/python3
 """
+cript that takes in a URL and an email address
 Displays response body with email passed to URL using POST request
 """
 import requests
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: {} <URL> <email>".format(sys.argv[0]))
-        sys.exit(1)
-
-    url = sys.argv[1]
-    email = sys.argv[2]
-
-    # Create a dictionary to hold the email parameter
-    data = {"email": email}
-
-    # Send a POST request to the provided URL with the email parameter
-    response = requests.post(url, data=data)
-
-    # Display the body of the response
-    print("Your email is:", response.text)
+    data = {'email': argv[2]}
+    url = argv[1]
+    r = requests.post(url, data=data)
+    print(r.text)
