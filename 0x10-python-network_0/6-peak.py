fe680ccc1e42c4dc20747251b5b46cf891a79eef
InfintyJer <89025169+InfinityJer@users.noreply.github.com>
2023-09-29 18:41:42 +0800

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,16 +4,24 @@ From unsorted intagers finds a peak.
 """
 
 
-def find_peak(list_of_integers):
-    if not list_of_integers:
-        return
-    return(search(0, len(list_of_integers) - 1, list_of_integers))
-
-
 def search(lo, h, ints):
+    """
+    searches peak.
+    """
+
     mid = (lo + h) // 2
     if lo == h:
         return ints[h]
     if ints[mid] < ints[mid + 1]:
         return(search(mid + 1, h, ints))
     return(search(lo, mid, ints))
+
+
+def find_peak(list_of_integers):
+    """
+    find the peak fro the search
+    """
+
+    if not list_of_integers:
+        return
+    return(search(0, len(list_of_integers) - 1, list_of_integers))
